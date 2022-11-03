37473f5d1c5a63b5204b474abae10cfa74d14c6d
xuhongwei <xuhongwei@popicorns.com>
2022-11-03 18:09:42 +0800

    1.接口配置改为使用类配置，方便索引配置 2.wms_robot和eta_robot接入新配置

--- script/stock_hot_set.py
+++ script/stock_hot_set.py
@@ -1,6 +1,6 @@
-from testcase import ims_logics
+from cases import ims_robot
 
 if __name__ == '__main__':
-    skus = ["DJ690WS210", "HWU72U2280"]
-
-    ims_logics.del_stock(skus)
+    # ims_robot.add_bom_stock('63203684930', 'B', 1, [257280], 566, 513)
+    # ims_robot.del_stock(['63203684930', '16338527895', '20537964151'])
+    ims_robot.add_bom_stock('J020053-11', 'A', 1, [1496], 513, 513)
