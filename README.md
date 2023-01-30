84be40ab2e0369185294226abe801d6b79f30a39
chung-jay <persistence_lin@163.com>
2023-01-30 16:42:50 +0800

    ftr: 修改readme；添加工具类方法

--- README.md
+++ README.md
@@ -3,36 +3,53 @@
     1.安装python，使用的是3.X版本
     2.安装依赖：pip install -r requirements.txt
     3.通过gitlab clone代码到本地
-    4.修改config目录下sys_config文件,配置要对接的环境和登录用户
+    4.修改config目录下 __init__.py 文件,配置要对接的环境和登录用户信息，具体如下：
+
+        在__init__.py文件中，写入以下内容：
+            from config.env_config import prefix_config, mysql_config
+            
+            # 指定环境：'test160'/'test25'/'test26'/'uat'
+            env = 填上方注释中的对应环境变量
+            
+            env_prefix_config = env_config.prefix_config.get(env)
+            db_config = env_config.mysql_config.get(env)
+            
+            user = {
+                'username': '', # 你的账号
+                'password': '' # 你的密码
+            }
 
 模块说明：
 
     - supply-test 根目录
-    - config 配置文件
-        - third_party_api_configs 接口配置文件夹，存放各系统接口配置文件
-        - env_config 系统配置文件，用于配置域名配置和数据库配置
-        - server_config 服务器账号、日志路径
-        - __init__ 账号配置、环境配置引入
-    - testcase 测试用例,基于api_request编排测试场景（预期废弃，迁移到cases）
-    - cases 测试用例,基于api_request编排测试场景
-    - data_generator 基于api_request编排业务流程，生成业务数据
-    - script 脚本文件夹，存放脚本
+        - config 配置文件
+            - third_party_api_configs 接口配置文件夹，存放各系统接口配置文件
+            - env_config 系统配置文件，用于配置域名配置和数据库配置
+            - server_config 服务器账号、日志路径
+            - __init__ 账号配置、环境配置引入
 
-    - logics 逻辑层，基于api_request编排业务逻辑
-    - db_operator 各系统数据库操作
-    - api_request 各系统接口原子封装
+        - testcase 测试用例,（预期废弃，迁移到cases）
+        - cases 测试用例,基于robots的业务行为方法编排测试场景
 
-    - utils 通用工具模块
-    - models 数据库表模型
-    - barcodes 条形码生成文件夹
-    - logs 日志文件夹
+        - data_generator 基于robots的业务行为方法编排业务，用于生成业务数据
+        - script 脚本文件夹，存放一些独立的测试脚本，如库存增删、xmind转化、语言包对比等测试脚本
 
+        - robots 逻辑层，封装调用api的方法，理论上每一个业务行为封装成独立的方法
+        - robot_run 基于robots的业务行为方法编排业务，用于跑各系统的业务全流程
 
-拓展说明：
-
-    1.在config下的api_config文件夹中修改或增加系统接口配置文件，增加或修改相关接口配置
-    2.在controller文件夹下增加或需改系统接口控制器，添加对应的接口调用逻辑
-    3.在testcase或script中编写对应的controller引用，实现对应测试用例或脚本功能
+        - dbo 各系统数据库操作
+        - utils 通用工具模块
+            - barcode_handler.py 生成条形码工具
+            - excel_handler.py  excel操作工具类
+            - log_handler.py  日志工具类
+            - rsa_handler.py  rsa加密工具
+            - ssh_handler.py  ssh 工具
+            - time_handler.py  日期时间工具类，封装了一些常用的日期时间转换方法
+            - wait_handler.py  延迟等待装饰器，可以用于异步业务场景自定义等待重试
+    
+        - models 数据库表模型类
+        - barcodes 条形码图片文件夹
+        - logs 日志文件夹
     
 
 model生成：
@@ -44,18 +61,4 @@ model生成：
     python3 -m pwiz -e mysql -H 10.0.0.127 -p 3306 -u erp -P supply_oms > models/oms_model.py
     python3 -m pwiz -e mysql -H 10.0.0.127 -p 3306 -u erp -P supply_scm > models/scm_model.py
 
-全局配置：
 
-    在config下新建__init__.py文件，写入以下内容：
-        from config.env_config import prefix_config, mysql_config
-        
-        # 指定环境：'test160'/'test25'/'test26'/'uat'
-        env = 填上方注释中的对应环境变量
-        
-        env_prefix_config = env_config.prefix_config.get(env)
-        db_config = env_config.mysql_config.get(env)
-        
-        user = {
-            'username': '', # 你的账号
-            'password': '' # 你的密码
-        }
---
+++ utils/time_handler.py
@@ -0,0 +1,283 @@
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+from typing import Union
+from datetime import datetime, date, time, timedelta
+
+
+DATETIME_LIKE_TYPE = Union[datetime, date, time, float, int, str]
+DATETIME_PARSE_FORMAT = [
+    "%Y-%m-%d %H:%M:%S",
+    "%Y-%m-%d %H:%M",
+    "%Y-%m-%d %H:%M:%SZ",
+    "%Y-%m-%d %H:%M:%S%z",
+    "%Y-%m-%d %H:%M:%S.%f%z",
+    "%Y-%m-%d %H:%M:%S.%f",
+    "%Y-%m-%d %H:%M:%S %f",
+    "%Y-%m-%d %H:%M:%S %f%z",
+    "%Y-%m-%d %H:%M:%S%Z",
+    "%c",
+    "%S",
+    "%Y-%m-%d",
+    # YMD other than ISO
+    "%Y%m%d",
+    "%Y.%m.%d",
+    # Popular MDY formats
+    "%m/%d/%Y",
+    "%m/%d/%y",
+    # DMY with full year
+    "%d %m %Y",
+    "%d-%m-%Y",
+    "%d/%m/%Y",
+    "%d/%m %Y",
+    "%d.%m.%Y",
+    "%d. %m. %Y",
+    "%d %b %Y",
+    "%d %B %Y",
+    "%d. %b %Y",
+    "%d. %B %Y",
+    # MDY with full year
+    "%b %d %Y",
+    "%b %dst %Y",
+    "%b %dnd %Y",
+    "%b %drd %Y",
+    "%b %dth %Y",
+    "%b %d, %Y",
+    "%b %dst, %Y",
+    "%b %dnd, %Y",
+    "%b %drd, %Y",
+    "%b %dth, %Y",
+    "%B %d %Y",
+    "%B %dst %Y",
+    "%B %dnd %Y",
+    "%B %drd %Y",
+    "%B %dth %Y",
+    "%B %d, %Y",
+    "%B %dst, %Y",
+    "%B %dnd, %Y",
+    "%B %drd, %Y",
+    "%B %dth, %Y",
+    # DMY with 2-digit year
+    "%d %m %y",
+    "%d-%m-%y",
+    "%d/%m/%y",
+    "%d/%m-%y",
+    "%d.%m.%y",
+    "%d. %m. %y",
+    "%d %b %y",
+    "%d %B %y",
+    "%d. %b %y",
+    "%d. %B %y",
+    # MDY with 2-digit year
+    "%b %dst %y",
+    "%b %dnd %y",
+    "%b %drd %y",
+    "%b %dth %y",
+    "%B %dst %y",
+    "%B %dnd %y",
+    "%B %drd %y",
+    "%B %dth %y"
+]
+
+
+class HumanDateTime(object):
+
+    @classmethod
+    def from_any(cls, any_dt: DATETIME_LIKE_TYPE):
+        """把传入的时间解析成datetime对象
+
+        :param any_dt: 可以是datetime、date、time对象，或秒级或毫秒时间戳，或字符串
+        :rtype: datetime
+        """
+        if any_dt is None:
+            return datetime.now().replace(microsecond=0)
+        elif isinstance(any_dt, datetime):
+            return any_dt
+        elif isinstance(any_dt, date):
+            return datetime(
+                year=any_dt.year, month=any_dt.month, day=any_dt.day
+            )
+        elif isinstance(any_dt, time):
+            return datetime.now().replace(
+                hour=any_dt.hour, minute=any_dt.minute, second=any_dt.second,
+                microsecond=any_dt.microsecond
+            )
+        elif isinstance(any_dt, (float, int)):
+            return cls.timestamp_parse(any_dt)
+        elif isinstance(any_dt, str):
+            try:
+                tt = float(any_dt)
+                return cls.timestamp_parse(tt)
+            except ValueError:
+                return cls.str_parse(any_dt)
+        else:
+            raise TypeError(f"Unknown datetime-like type: {type(any_dt)}")
+
+    @classmethod
+    def str_parse(cls, dt_str):
+        """把传入的字符串成datetime对象，1个字符串可能符合多种解析格式，因此这里的转换可能与预期不符合
+
+        :param str dt_str: 需要解析成datetime的字符串
+        :rtype: datetime
+        """
+        for fmt in DATETIME_PARSE_FORMAT:
+            try:
+                return datetime.strptime(dt_str, fmt)
+            except ValueError:
+                pass
+        else:
+            raise ValueError(f'输入的时间格式不正确：{dt_str}')
+
+    @classmethod
+    def timestamp_parse(cls, dt_num: Union[float, int]):
+        """把传入的时间戳成datetime对象
+
+        :param dt_num: 需要解析成datetime的时间戳
+        :rtype: datetime
+        """
+        if 100000000 <= dt_num < 100000000000:
+            return datetime.fromtimestamp(dt_num)
+        elif 100000000000 <= dt_num < 100000000000000:
+            return datetime.fromtimestamp(dt_num / 1000)
+        else:
+            raise ValueError(f'时间戳超出范围：{dt_num}')
+
+    @classmethod
+    def today(cls):
+        """今天零点
+
+        :rtype: HumanDateTime
+        """
+        return cls(datetime.now().replace(
+            hour=0, minute=0, second=0, microsecond=0
+        ))
+
+    @classmethod
+    def yesterday(cls):
+        """昨天零点
+
+        :rtype: HumanDateTime
+        """
+        return cls.today().sub(days=1)
+
+    @classmethod
+    def tomorrow(cls):
+        """明天零点
+        :rtype: HumanDateTime
+        """
+        return cls.today().add(days=1)
+
+    def __init__(self, origin: Union[DATETIME_LIKE_TYPE, HumanDateTime] = None):
+        """
+        :param origin: 可以是HumanDateTime、datetime、date、time对象，或秒级或毫秒时间戳，或字符串
+        """
+        if isinstance(origin, HumanDateTime):
+            self.origin = origin.origin
+            self.dt: datetime = origin.dt
+        else:
+            self.origin = origin
+            self.dt: datetime = self.from_any(self.origin)
+
+    def human_time(self, fmt='%Y-%m-%d %H:%M:%S'):
+        """返回日期时间的格式化字符串
+
+        :param str fmt: 字符串格式
+        :rtype: str
+        """
+        return self.dt.strftime(fmt)
+
+    def human_date(self, fmt='%Y-%m-%d'):
+        """返回日期的格式化字符串
+
+        :param str fmt: 字符串格式
+        :rtype: str
+        """
+        return self.dt.date().strftime(fmt)
+
+    def timestamp(self):
+        """返回秒级时间戳
+
+        :rtype: int
+        """
+        return int(self.dt.timestamp())
+
+    def milli_timestamp(self):
+        """返回毫秒级时间戳
+
+        :rtype: int
+        """
+        return int(self.dt.timestamp() * 1000)
+
+    def micro_timestamp(self):
+        """返回微秒级时间戳
+
+        :rtype: int
+        """
+        return int(self.dt.timestamp() * 1000000)
+
+    def add(self, days=0, weeks=0, hours=0, minutes=0, seconds=0):
+        """时间加法
+
+        :param int days: 天数
+        :param int weeks: 周数
+        :param int hours: 小时数
+        :param int minutes: 分钟数
+        :param int seconds: 秒数
+        :rtype: HumanDateTime
+        """
+        return type(self)(
+            self.dt + timedelta(
+                days=days, weeks=weeks,
+                hours=hours, minutes=minutes, seconds=seconds
+            )
+        )
+
+    def sub(self, days=0, weeks=0, hours=0, minutes=0, seconds=0):
+        """时间减法
+
+        :param int days: 天数
+        :param int weeks: 周数
+        :param int hours: 小时数
+        :param int minutes: 分钟数
+        :param int seconds: 秒数
+        :rtype: HumanDateTime
+        """
+        return self.add(
+            days=-days, weeks=-weeks,
+            hours=-hours, minutes=-minutes, seconds=-seconds
+        )
+
+    def approach(self, any_dt: Union[DATETIME_LIKE_TYPE, HumanDateTime], seconds):
+        """检查是否在误差范围内
+
+        :param any_dt: 可以是datetime、date、time对象，或秒级或毫秒时间戳，或字符串
+        :param int seconds: 秒数
+        :rtype: bool
+        """
+        return abs(self.dt.timestamp() - type(self)(any_dt).dt.timestamp()) <= seconds
+
+    def __lt__(self, other: Union[DATETIME_LIKE_TYPE, HumanDateTime]):
+        return self.dt.timestamp() < type(self)(other).dt.timestamp()
+
+    def __le__(self, other: Union[DATETIME_LIKE_TYPE, HumanDateTime]):
+        return self.dt.timestamp() <= type(self)(other).dt.timestamp()
+
+    def __gt__(self, other: Union[DATETIME_LIKE_TYPE, HumanDateTime]):
+        return self.dt.timestamp() > type(self)(other).dt.timestamp()
+
+    def __ge__(self, other: Union[DATETIME_LIKE_TYPE, HumanDateTime]):
+        return self.dt.timestamp() >= type(self)(other).dt.timestamp()
+
+    def __eq__(self, other: Union[DATETIME_LIKE_TYPE, HumanDateTime]):
+        return self.dt.timestamp() == type(self)(other).dt.timestamp()
+
+    def __ne__(self, other: Union[DATETIME_LIKE_TYPE, HumanDateTime]):
+        return self.dt.timestamp() != type(self)(other).dt.timestamp()
+
+    def __str__(self):
+        return self.human_time()
+
+    def __getattr__(self, item):
+        return getattr(self.dt, item)
+
+    def __repr__(self):
+        return f"HumanDateTime(origin={repr(self.origin)}, dt={repr(self.dt)})"
