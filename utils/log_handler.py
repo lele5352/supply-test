f5c29e37d1a808d5714b0a9be38cd19c3d59fdd1
chung-jay <persistence_lin@163.com>
2023-02-02 19:27:48 +0800

    ftr: 日志配置

--- utils/log_handler.py
+++ utils/log_handler.py
@@ -1,12 +1,20 @@
 from loguru import logger
 
 log_level = 'INFO'
+all_path = "../logs/all_{time:YYYY-MM-DD}.log"
+error_path = "../logs/error_{time:YYYY-MM-DD}.log"
 
-logger.add("../logs/all_{time:YYYY-MM-DD}.log",
-           format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {message}",
-           level=log_level)
+try:
+    from config import console_log
+except ImportError:
+    console_log = False
 
-logger.add("../logs/error_{time:YYYY-MM-DD}.log",
-           format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {message}",
-           level='ERROR')
+if not console_log:
+    logger.remove(handler_id=None)
+
+logger.add(all_path, format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {message}", level=log_level)
+logger.add(error_path, format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {message}", level='ERROR')
+
+
+
 
