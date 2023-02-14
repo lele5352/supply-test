0ae629dfccce5fca7225822f25e89dd7c2db65c0
xuhongwei <xuhongwei@popicorns.com>
2023-02-14 10:35:01 +0800

    fix：优化请求

--- robots/robot.py
+++ robots/robot.py
@@ -15,47 +15,51 @@ class Robot:
     定义基础机器人
     """
 
-    def __init__(self, prefix, headers):
-        self.prefix = prefix
+    def __init__(self, prefix=None, headers=None):
         self.headers = headers
-
-    @classmethod
-    def content_parse(cls, uri_path, method, data):
-        return {
-            "uri_path": uri_path,
-            "method": method,
-            "data": data
-        }
+        self.prefix = prefix
 
     def call_api(self, uri_path, method, data=None, files=None) -> dict:
-        req_url = urljoin(self.prefix, uri_path)
-
-        if method == "post":
-            res = requests.post(req_url, headers=self.headers, json=data, files=files)
-        elif method == "get":
-            res = requests.get(req_url, headers=self.headers, params=data)
+        url = urljoin(self.prefix, uri_path)
+        if method == "get":
+            response = requests.get(url, headers=self.headers)
+        elif method == "post":
+            response = requests.post(url, json=data, headers=self.headers, files=files)
         elif method == "put":
-            res = requests.put(req_url, headers=self.headers, json=data)
+            response = requests.put(url, json=data, headers=self.headers)
+        elif method == "delete":
+            response = requests.delete(url, headers=self.headers)
         else:
-            return {}
+            raise ValueError("Invalid request method")
 
-        result_data = res.json()
         log.info("请求头：%s" % json.dumps(self.headers, ensure_ascii=False))
-        log.info("请求内容：%s" % json.dumps({"method": method, "url": req_url, "data": data}, ensure_ascii=False))
-        log.info("响应内容：" + json.dumps(result_data, ensure_ascii=False))
+        log.info("请求内容：%s" % json.dumps({"method": method, "url": url, "data": data}, ensure_ascii=False))
+        log.info("响应内容：" + json.dumps(response.json(), ensure_ascii=False))
         log.info(
             "-------------------------------------------------我是分隔符-------------------------------------------------")
-        return result_data
+        return response.json()
+
+    @classmethod
+    def is_data_empty(cls, response_data):
+        if not response_data:
+            return True
+        elif response_data.get("data", None) is None:
+            return True
+        elif len(response_data.get("data").get("records")) == 0:
+            return True
+        else:
+            return False
 
     @classmethod
     def formatted_result(cls, res_data):
         if res_data["code"] == 200:
             return cls.report(1, "操作成功,msg:%s" % res_data["message"], res_data["data"])
+        elif res_data.get("data") and res_data.get("message"):
+            return cls.report(0, "操作失败,msg:%s" % res_data["message"], res_data["data"])
+        elif res_data.get("message"):
+            return cls.report(0, "操作失败,msg:%s" % res_data["message"], None)
         else:
-            if res_data.get("data"):
-                return cls.report(0, "操作失败,msg:%s" % res_data["message"], res_data["data"])
-            else:
-                return cls.report(0, "操作失败,msg:%s" % res_data["message"], None)
+            return cls.report(0, "操作失败", None)
 
     @classmethod
     def report(cls, code, msg, data):
