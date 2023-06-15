d408af8785e47fc6e0d15e060d1ef55e25f114ce
xuhongwei <xuhongwei@popicorns.com>
2023-06-15 10:28:32 +0800

    抽离公共方法

--- utils/tms_cal_items.py
+++ utils/tms_cal_items.py
@@ -58,6 +58,64 @@ class TMSCalcItems:
         return self.weight / self.volume() * 1728
 
 
+class GoodsMeasurementItems:
+    """货物计量项信息"""
+
+    def __init__(self, goods_info, goods_unit, channel_unit, weight_rounding, weight_precision, size_rounding,
+                 size_precision):
+        self.source_unit = goods_unit
+        self.target_unit = channel_unit
+        self.weight_rounding = weight_rounding
+        self.weight_precision = weight_precision
+        self.size_rounding = size_rounding
+        self.size_precision = size_precision
+        self.goods_info = goods_info
+        self.tms_items = TMSCalcItems(**self.goods_info)
+
+    def origin_items(self):
+        return {
+            "重量": {"num": self.goods_info.get("weight"), "num_type": "weight"},
+            "最长边": {"num": self.tms_items.longest_side(), "num_type": "size"},
+            "次长边": {"num": self.tms_items.mid_side(), "num_type": "size"},
+            "最短边": {"num": self.tms_items.shortest_side(), "num_type": "size"},
+            "围长": {"num": self.tms_items.girth(), "num_type": "size"},
+            "周长": {"num": self.tms_items.perimeter(), "num_type": "size"},
+            "两边长": {"num": self.tms_items.two_sides_length(), "num_type": "size"},
+            "体积": {"num": self.tms_items.volume(), "num_type": "volume"}
+        }
+
+    def unit_changed_items(self):
+        return {
+            x: UnitChange.change(
+                self.origin_items().get(x).get("num"),
+                self.origin_items().get(x).get("num_type"),
+                self.source_unit, self.target_unit)
+            for x in self.origin_items()
+        }
+
+    def rounded_result(self):
+        temp_dict = dict()
+
+        for item in self.unit_changed_items():
+            if item == "重量":
+                if self.weight_rounding == "向上取整":
+                    temp_dict[item] = Rounding.round_up(self.unit_changed_items().get(item), self.weight_precision)
+                elif self.weight_rounding == "向下取整":
+                    temp_dict[item] = Rounding.round_down(self.unit_changed_items().get(item), self.weight_precision)
+                else:
+                    temp_dict[item] = Rounding.round_half_up(self.unit_changed_items().get(item),
+                                                             self.weight_precision)
+            else:
+                if self.size_rounding == "向上取整":
+                    temp_dict[item] = Rounding.round_up(self.unit_changed_items().get(item), self.size_precision)
+
+                elif self.size_rounding == "向下取整":
+                    temp_dict[item] = Rounding.round_down(self.unit_changed_items().get(item), self.size_precision)
+                else:
+                    temp_dict[item] = Rounding.round_half_up(self.unit_changed_items().get(item), self.size_precision)
+        return temp_dict
+
+
 def package_calc(goods_info_list, precision):
     temp_result = [0, 0, 0]
     temp_weight = 0
