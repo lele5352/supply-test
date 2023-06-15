b55199ee32519fdf0a30dad9aad2bdae9ba32218
xuhongwei <xuhongwei@popicorns.com>
2023-06-15 10:27:50 +0800

    抽离公共方法

--- utils/tms_cal_items.py
+++ utils/tms_cal_items.py
@@ -1,90 +1,5 @@
-import decimal
-
-
-class Rounding:
-    """数值取整"""
-
-    @classmethod
-    def round_up(cls, num, precision):
-        num_decimal = decimal.Decimal(str(num))
-        precision_decimal = decimal.Decimal(str(precision))
-        factor = decimal.Decimal(1) / precision_decimal
-        rounded_decimal = (num_decimal * factor).to_integral_value(rounding=decimal.ROUND_CEILING)
-        rounded_decimal /= factor
-        rounded_decimal = rounded_decimal.quantize(precision_decimal, rounding=decimal.ROUND_CEILING)
-        return rounded_decimal
-
-    @classmethod
-    def round_down(cls, num, precision):
-        num_decimal = decimal.Decimal(str(num))
-        precision_decimal = decimal.Decimal(str(precision))
-        factor = decimal.Decimal(1) / precision_decimal
-        rounded_decimal = (num_decimal * factor).to_integral_value(rounding=decimal.ROUND_FLOOR)
-        rounded_decimal /= factor
-        rounded_decimal = rounded_decimal.quantize(precision_decimal, rounding=decimal.ROUND_FLOOR)
-        return rounded_decimal
-
-    @classmethod
-    def round_half_up(cls, num, precision):
-        num_decimal = decimal.Decimal(str(num))
-        precision_decimal = decimal.Decimal(str(precision))
-        factor = decimal.Decimal(1) / precision_decimal
-        rounded_decimal = (num_decimal * factor).to_integral_value(rounding=decimal.ROUND_HALF_EVEN)
-        rounded_decimal /= factor
-        rounded_decimal = rounded_decimal.quantize(precision_decimal, rounding=decimal.ROUND_HALF_EVEN)
-        return rounded_decimal
-
-
-class UnitChange:
-    @classmethod
-    def cm_to_in(cls, number):
-        return number * 0.393700787402
-
-    @classmethod
-    def cm3_to_in3(cls, number):
-        return number * 0.0610238
-
-    @classmethod
-    def kg_to_lb(cls, number):
-        return number * 2.204622
-
-    @classmethod
-    def in_to_cm(cls, number):
-        return number * 2.539950
-
-    @classmethod
-    def in3_to_cm3(cls, number):
-        return number * 16.387037
-
-    @classmethod
-    def lb_to_kg(cls, number):
-        return number * 0.453592
-
-    @classmethod
-    def change(cls, num, num_type, source_unit, target_unit):
-        """执行转换"""
-        if source_unit == target_unit:
-            return num
-        elif target_unit == "gj":
-            if num_type == "size":
-                return cls.in_to_cm(num)
-            elif num_type == "weight":
-                return cls.lb_to_kg(num)
-            elif num_type == "volume":
-                return cls.in3_to_cm3(num)
-            else:
-                return "error"
-        elif target_unit == "yz":
-            if num_type == "size":
-                return cls.cm_to_in(num)
-            elif num_type == "weight":
-                return cls.kg_to_lb(num)
-            elif num_type == "volume":
-                return cls.cm3_to_in3(num)
-            else:
-                return "error"
-        else:
-            return "error"
+from utils.rounding_handler import *
+from utils.unit_change_handler import *
 
 
 class TMSCalcItems:
@@ -128,7 +43,7 @@ class TMSCalcItems:
         """体积=长*宽*高"""
         return self.longest_side() * self.mid_side() * self.shortest_side()
 
-    def volume_weight(self,precision ):
+    def volume_weight(self, precision):
         """体积重=(长*宽*高)/体积重系数"""
         return round(self.volume() / precision, 2)
 
@@ -143,7 +58,7 @@ class TMSCalcItems:
         return self.weight / self.volume() * 1728
 
 
-def package_calc(goods_info_list,precision):
+def package_calc(goods_info_list, precision):
     temp_result = [0, 0, 0]
     temp_weight = 0
     for length, width, height, weight in goods_info_list:
@@ -163,15 +78,6 @@ def package_calc(goods_info_list,precision):
 
 if __name__ == '__main__':
     items = TMSCalcItems(24.31, 66.6, 44.4, 199.9)
-    # print("grith:{}".format(Rounding.round_up(items.girth(), 1)))
-    # print("longestEdge:{}".format(Rounding.round_up(items.longest_side(), 1)))
-    # print("perimeter:{}".format(Rounding.round_up(items.perimeter(), 1)))
-    # print("secondSide:{}".format(Rounding.round_up(items.mid_side(), 1)))
-    # print("shortestSide:{}".format(Rounding.round_up(items.shortest_side(), 1)))
-    # print("sideLength:{}".format(Rounding.round_up(items.two_sides_length(), 1)))
-    # print("volume:{}".format(Rounding.round_up(items.volume(), 1)))
-    # print("weight:{}".format(Rounding.round_up(items.weight, 1)))
-    # print("volume_weight:{}".format(round(items.volume_weight(), 2)))
     no_pack_goods_list = [
         (22.2, 99.9, 33.3, 9.99),
         (22.2, 55.5, 33.3, 1.22),
@@ -186,5 +92,5 @@ if __name__ == '__main__':
         (22.2, 55.5, 33.3, 1.22)
 
     ]
-    print(package_calc(no_pack_goods_list,2000))
-    print(package_calc(pack_goods_list,2200))
+    print(package_calc(no_pack_goods_list, 2000))
+    print(package_calc(pack_goods_list, 2200))
