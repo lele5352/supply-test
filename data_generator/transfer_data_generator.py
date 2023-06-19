bbe3407bd4bb9daac7fd5767677f35f1bb5ae258
Vincent <wenhao@popicorns.com>
2023-06-19 18:21:39 +0800

    优化单次bind多个拣货单到一个发货单中

--- data_generator/transfer_data_generator.py
+++ data_generator/transfer_data_generator.py
@@ -1,5 +1,7 @@
 import random
 import time
+import concurrent.futures
+
 
 from cases import *
 
@@ -134,7 +136,7 @@ class WmsTransferDataGenerator:
         print("创建调拨出库成功,调拨交接单号：%s" % order_no)
         return order_no
 
-    def create_cabinet_order(self, trans_out_id, trans_out_to_id, trans_in_id, trans_in_to_id, sale_sku_code, bom,
+    def create_cabinet_order(self, trans_out_id, trans_out_to_id, trans_in_id, trans_in_to_id, sale_sku_code_list, bom,
                              demand_qty, demand_type=1, customer_type=1, remark=''):
         """
         生成海柜单
@@ -143,38 +145,46 @@ class WmsTransferDataGenerator:
         :param int trans_in_id: 调入仓库id
         :param any trans_out_to_id: 调出仓库的目的仓id，仅调出仓为中转仓时必填
         :param any trans_in_to_id: 调入仓库的目的仓id，仅调入仓为中转仓时必填
-        :param string sale_sku_code: 调拨的商品的销售sku
+        :param list sale_sku_code_list: 调拨的商品的销售sku列表
         :param int demand_qty: 调拨数量
         :param int demand_type: 调拨类型
         :param int customer_type: 客户类型：1-普通客户；2-大客户
         :param string remark: 备注
         :param bom: bom版本
         """
+        def bind_sub_pick(sku_code):
+            demand_no = self.create_transfer_demand(
+                trans_out_id, trans_out_to_id, trans_in_id, trans_in_to_id, sku_code, bom, demand_qty, demand_type,
+                customer_type, remark
+            )
+            if not demand_no:
+                print("创建调拨拣货单失败")
+                return
+            # 执行调拨流程到扫货绑定交接单
+            result, order_no = run_transfer(demand_no, "bind")
+            return order_no
         # 生成调拨需求
-        demand_no = self.create_transfer_demand(
-            trans_out_id, trans_out_to_id, trans_in_id, trans_in_to_id, sale_sku_code, bom, demand_qty, demand_type,
-            customer_type, remark
-        )
-
-        if not demand_no:
-            print('创建调拨拣货单失败')
-            return
-        # 执行调拨流程到复核完成
-        result, order_no = run_transfer(demand_no, "bind")
-        # print("复核成功,交接单号：%s" % order_no)
-        # 获取交接货单id
-        order_detail = self.wms_app.transfer_handover_order(handover_ids=[order_no]).get('data').get('records')[0]
-        handover_id = order_detail.get('id')
-        # 获取海运柜号信息并更新
-        cabinet_info = random.choice(self.wms_app.transfer_cabinet_list().get('data'))
-        container_no = cabinet_info.get('cabinetNumber')
-        so_number = cabinet_info.get('soNumber')
-        # print("更新海运柜号，{}".format(cabinet_info))
-        self.wms_app.transfer_out_update_delivery_config(handover_id, container_no, so_number)
-        # print("完成调拨发货")
-        self.wms_app.transfer_out_delivery(order_no)
-        print('交接单号:{},交接单id:{},海柜信息:{}'.format(order_no, handover_id, cabinet_info))
-        return order_no
+        with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+            to_do = []
+            order_no = ''
+            for sale_sku_code in sale_sku_code_list:  # 模拟多个任务
+                future = executor.submit(bind_sub_pick, sale_sku_code)
+                to_do.append(future)
+            for future in concurrent.futures.as_completed(to_do):  # 并发执行
+                order_no = future.result()
+            # 获取交接货单id
+            order_detail = self.wms_app.transfer_handover_order(handover_ids=[order_no]).get('data').get('records')[0]
+            handover_id = order_detail.get('id')
+            # 获取海运柜号信息并更新
+            cabinet_info = random.choice(self.wms_app.transfer_cabinet_list().get('data'))
+            container_no = cabinet_info.get('cabinetNumber')
+            so_number = cabinet_info.get('soNumber')
+            # print("更新海运柜号，{}".format(cabinet_info))
+            self.wms_app.transfer_out_update_delivery_config(handover_id, container_no, so_number)
+            # print("完成调拨发货")
+            self.wms_app.transfer_out_delivery(order_no)
+            print('交接单号:{},交接单id:{},海柜信息:{}'.format(order_no, handover_id, cabinet_info))
+            return order_no
 
 
 if __name__ == '__main__':
@@ -184,16 +194,6 @@ if __name__ == '__main__':
     # transfer_data.create_transfer_demand(511, 513, 513, 513, '63203684930', "B", 1)
     # transfer_data.create_handover_order(512, '', 513, 513, '63203684930', "B", 1)
     # transfer_data.create_transfer_pick_order(512, '', 513, 513, '63203684930',"B", 2)
-    import threading
-
     # sku_list = ['JF067T801S', 'JF31665XD8', 'JF954856UJ', 'JF389G7H75', 'P31559628', 'BPJF067T801SB01']
-    sku_list = ['JF067T801S', 'JF31665XD8']
-    # sku_list = ['JF067T801S']
-    th_list = []
-    for sku in sku_list:
-        th = threading.Thread(target=transfer_data.create_cabinet_order, args=(
-            515, 515, 542, 542, sku, "A", 1))
-        th.start()
-        th_list.append(th)
-    for th in th_list:
-        th.join()
+    sku_list = ['JF067T801S', 'JF31665XD8','JF067T801S', 'JF31665XD8','JF067T801S', 'JF31665XD8','JF067T801S', 'JF31665XD8','JF067T801S', 'JF31665XD8']
+    transfer_data.create_cabinet_order(515, 515, 542, 542, sku_list, "A", 1)
