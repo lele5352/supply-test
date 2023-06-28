abd20e9340ca2f9a5901c835e6bcd15b7975f62e
Vincent <wenhao@popicorns.com>
2023-06-28 11:01:54 +0800

    整合创建海柜单方法

--- data_generator/transfer_data_generator.py
+++ data_generator/transfer_data_generator.py
@@ -136,7 +136,7 @@ class WmsTransferDataGenerator:
         return order_no
 
     def create_cabinet_order(self, trans_out_id, trans_out_to_id, trans_in_id, trans_in_to_id, sale_sku_code_list, bom,
-                             demand_qty, demand_type=1, customer_type=1, remark='', process_flag='bind'):
+                             demand_qty, demand_type=1, customer_type=1, remark=''):
         """
         生成海柜单
 
@@ -150,7 +150,6 @@ class WmsTransferDataGenerator:
         :param int customer_type: 客户类型：1-普通客户；2-大客户
         :param string remark: 备注
         :param bom: bom版本
-        :param process_flag: 流程标志位： bind，submit
         """
 
         def bind_sub_pick(sku_code):
@@ -177,6 +176,12 @@ class WmsTransferDataGenerator:
             return order_no
 
     def submit_cabinet_order(self, order_no, container_no=None, so_number=None):
+        """
+        提交发货交接单
+        :param int order_no: 交接单号
+        :param int container_no: 默认自动获取空闲海柜，指定该值将会拼柜（cds海柜单未发柜前有效）
+        :param int so_number: 默认自动获取订舱号，与海柜号绑定
+        """
         # 获取交接货单id
         order_detail = self.wms_app.transfer_handover_order(handover_ids=[order_no]).get('data').get('records')[0]
         handover_id = order_detail.get('id')
@@ -190,71 +195,47 @@ class WmsTransferDataGenerator:
         print('交接单号:{},交接单id:{},海柜信息:{}:{}'.format(order_no, handover_id, container_no, so_number))
         return container_no, so_number
 
+    @staticmethod
+    def generate_cds_cabin_order(sku_list, step=12, container_no=None, so_number=None, trans_out_id=542, trans_in_id=539):
+        """
+        生成退税海柜单
+        :param list[list[str]] sku_list: 需要发柜的sku
+        :param int step: 每个交接单的sku个数，list不可超过15个sku，否则会导致wms发货接口死锁
+        :param int container_no: 默认自动获取空闲海柜，指定该值将会拼柜（cds海柜单未发柜前有效）
+        :param int so_number: 默认自动获取订舱号，与海柜号绑定
+        :param int trans_out_id: 发货仓id
+        :param int trans_in_id: 收货仓id
+        """
+        for i in range(0, len(sku_list), step if step <= 15 else 15):
+            # 自动将大批量sku拆解为15个箱单一组的交接单，并自动进行拼柜处理
+            sec_sku_list = sku_list[i:i + step - 1]
+            order = transfer_data.create_cabinet_order(
+                trans_out_id, trans_out_id, trans_in_id, trans_in_id, sec_sku_list, "A", 1)
+            container_no, so_number = transfer_data.submit_cabinet_order(order, container_no, so_number)
+
 
 if __name__ == '__main__':
     transfer_data = WmsTransferDataGenerator()
-    # sku_list = ['JF067T801S', 'JF31665XD8', 'JF954856UJ', 'JF389G7H75', 'P31559628', 'BPJF067T801SB01']
-    """
-    收货仓：
-    广州1号（warehouseId: 594, warehouseCode: "GZFH01"），退税类型=不限
-    新泽西1号仓（warehouseId: 543, warehouseCode: "USNJ01"），退税类型=非退税
-    英国1号仓（warehouseId: 539, warehouseCode: "UKBH01"），退税类型=退税
-
-    发货仓：
-    洛杉矶1号仓，warehouseId: 542, warehouseCode: "USLA01"
-
-    非退税商品：
-    销售SKU：
-    JF067T801S,供应商：coco测试
-    JF31665XD8,供应商：coco测试
-
-    零部件商品：
-    P52601628 关联的SKU：JF067T801S 供应商：coco测试
-
-    退税商品：
-    销售SKU：
-    JF954856UJ,供应商：coco雪1
-    JF389G7H75,供应商：coco雪1
-
-    零部件商品：
-    P31559628   关联的SKU：JF954856UJ 供应商：coco雪1
-
-    内部BOM，供应商为空：
-    BPJF067T801SB01
-    """
-    # sku_list = [['JF067T801S', 'JF31665XD8', 'JF954856UJ', 'JF389G7H75', 'P31559628', 'P52601628']]
-    # sku_list = [['JF954856UJ', 'JF389G7H75', 'JF68Z9301V']]
-    # sku_list = [['JF68Z9301V']]
-    sku_list = [['BP77593016151B01']]
-    sku_list = [
-     ['BP77593016151B01','JF067T801S', 'JF31665XD8', 'JF954856UJ', 'JF389G7H75', 'P31559628', 'P52601628','JF954856UJ', 'JF389G7H75', 'JF68Z9301V'],
-     ['JF067T801S', 'JF31665XD8', 'JF40130HR7', 'JFW95751S0', 'HWIT187548', 'HW9W44M768', 'HW332S63I7', 'HW1711P3M7',],
-     ['HW203YE645', 'JFH4V76784', 'JFP171H106', 'JF8I4R7270', 'JFW5G63036', 'JF434R46Q4', 'JJ5E8055G4', 'JJS5B07625',],
-     ['ZSF30624L5', 'JJ528061NA', 'JJU389856V', 'PAN38335IL2', 'PAN09X7N757', 'PAN72H343A7', 'PAN004183AF',],
-     ['PAN96737UO5', 'PAN03HU1744', 'PAN3N38E090', 'PANY00695I5', 'PAN64T4883J', 'HWG2X83063', 'HW05N7T199',],
-     ['HWD833074R', 'JF570DY209', 'JF973Q4C44', 'JF57650TN8', 'JF02FX6668', 'JJ85990HZ4', 'JJ22155EF1', 'DJA9828C16',],
-     ['DJY9895U92', 'DJP352954J', 'DJUT880040', 'JF5891A6J4', 'JJ22V28I66', 'JFQ17267D2', 'JF5F11Z633', 'JF128Z202I',],
-     ['JF71Y908Q2', 'JFP07D6867', 'JF48Z2691G', 'PRE1000001', 'JJPY283153', 'JFGU822634', 'JF2KB93311', 'PRE_TEST4',],
-     ['PRE_TEST3', 'PRE_TEST2', 'PRE_TEST1', 'HANGE_PRE7', 'HANGE_PRE6', 'HANGE_PRE5', 'HANGE_PRE4', 'PRE_THREE',],
-     ['PRE_TWO', 'PRE_ONE', 'SKU_ATTR_DEFAULT', 'HANGE_PRE_NEW', 'HANGE_PRE', 'DJ690WS210', 'JFN124045S', 'JFZ06986D3',],
-     ['HWU72U2280', 'JF710294KC', 'JF002Z8F55', 'JF902K38B1', 'JFR06Y0103', 'JJ8L7B1005', 'JJ454072GE', 'JJ52L6C245',],
-     ['JF06728I8T', 'JF5D41G211', 'JJ2PI66951', 'JFL2847K46', 'JF9496MS19', 'JF86PM7085', 'JF2181O7E9', 'JF20O7A332',],
-     ['HW691E9F15', 'JF698LM501', 'JJH22251X0', 'JJ253R4X93', 'JJ77QH3613', 'JJJ3E57318', 'JJ8SM72435', 'JJ9R0Q1900',],
-     ['JF5555U5A3', 'JFMD427628', 'JF4P1582W3', 'JJ95900O1F', 'JF36567JT9', 'JFW28445H5', 'JF6300R13H', 'JF27P1419G',],
-     ['JF8T3M4789', 'JJJ517', 'GGG517', 'TESTNEW517', 'TEST517', 'JF091H128H', 'JF73231T5W', 'JF31564K2R', 'JF408PD244',],
-     ['JF0W26C942', 'JF1C2Q4008', 'HW372W2N04', 'JF07701PJ7', 'JF830537IK', 'JF681I4F73', 'WY8219Q39W', 'WY540T946Q',],
-     ['WY411H82M6', 'WY21M267E8', 'HW13835ZH0', 'HW3I128N68', 'HWZ6852N41', 'HW7222G4F0', 'JF32H16W70', 'JF2Q7538E0',],
-     ['JFF582Y514', 'JFL96792J1', 'JJW56O3441', 'JJ06Z0S128', 'JJ700B9Y45', 'JJ8322D5K2', 'JF94I4230E', 'JJ4368K46M',],
-     ['JJG1203V17', 'JF88X641Q4', 'JF81F69G03', 'JJ0895GR64', 'JF7R6497Z8', 'JF2224L1G4', 'JF0561XP63', 'JF8P629R88',],
-     ['JF4F17929R', 'JFN65Y6867', 'JFY30S9478', 'JF6620J22G', 'JJG802I913', 'JJZ064416Q', 'JJ0G1431T0']
+    multi_sku_list = [
+        'JF067T801S', 'JF31665XD8', 'JF954856UJ', 'JF389G7H75', 'P52601628', 'JF954856UJ', 'JF389G7H75', 'JF68Z9301V',
+        'JF067T801S', 'JF31665XD8', 'JF40130HR7', 'JFW95751S0', 'HWIT187548', 'HW9W44M768', 'HW332S63I7', 'HW1711P3M7',
+        'HW203YE645', 'JFH4V76784', 'JFP171H106', 'JF8I4R7270', 'JFW5G63036', 'JF434R46Q4', 'JJ5E8055G4', 'JJS5B07625',
+        'ZSF30624L5', 'JJ528061NA', 'JJU389856V', 'PAN38335IL2', 'PAN09X7N757', 'PAN72H343A7', 'PAN004183AF',
+        'PAN96737UO5', 'PAN03HU1744', 'PAN3N38E090', 'PANY00695I5', 'PAN64T4883J', 'HWG2X83063', 'HW05N7T199',
+        'HWD833074R', 'JF570DY209', 'JF973Q4C44', 'JF57650TN8', 'JF02FX6668', 'JJ85990HZ4', 'JJ22155EF1', 'DJA9828C16',
+        'DJY9895U92', 'DJP352954J', 'DJUT880040', 'JF5891A6J4', 'JJ22V28I66', 'JFQ17267D2', 'JF5F11Z633', 'JF128Z202I',
+        'JF71Y908Q2', 'JFP07D6867', 'JF48Z2691G', 'PRE1000001', 'JJPY283153', 'JFGU822634', 'JF2KB93311', 'PRE_TEST4',
+        'PRE_TEST3', 'PRE_TEST2', 'PRE_TEST1', 'HANGE_PRE7', 'HANGE_PRE6', 'HANGE_PRE5', 'HANGE_PRE4', 'PRE_THREE',
+        'PRE_TWO', 'PRE_ONE', 'SKU_ATTR_DEFAULT', 'HANGE_PRE_NEW', 'HANGE_PRE', 'DJ690WS210', 'JFN124045S',
+        'JFZ06986D3', 'HWU72U2280', 'JF710294KC', 'JF002Z8F55', 'JF902K38B1', 'JFR06Y0103', 'JJ8L7B1005', 'JJ454072GE',
+        'JJ52L6C245', 'JF06728I8T', 'JF5D41G211', 'JJ2PI66951', 'JFL2847K46', 'JF9496MS19', 'JF86PM7085', 'JF2181O7E9',
+        'JF20O7A332', 'HW691E9F15', 'JF698LM501', 'JJH22251X0', 'JJ253R4X93', 'JJ77QH3613', 'JJJ3E57318', 'JJ8SM72435',
+        'JJ9R0Q1900', 'JF5555U5A3', 'JFMD427628', 'JF4P1582W3', 'JJ95900O1F', 'JF36567JT9', 'JFW28445H5', 'JF6300R13H',
+        'JF27P1419G', 'JF8T3M4789', 'GGG517', 'TESTNEW517', 'TEST517', 'JF091H128H', 'JF73231T5W', 'JF31564K2R',
+        'JF408PD244', 'JF0W26C942', 'JF1C2Q4008', 'HW372W2N04', 'JF07701PJ7', 'JF830537IK', 'JF681I4F73', 'WY8219Q39W',
+        'WY540T946Q', 'WY411H82M6', 'WY21M267E8', 'HW13835ZH0', 'HW3I128N68', 'HWZ6852N41', 'HW7222G4F0', 'JF32H16W70',
+        'JF2Q7538E0', 'JFF582Y514', 'JFL96792J1', 'JJW56O3441', 'JJ06Z0S128', 'JJ700B9Y45', 'JJ8322D5K2', 'JF94I4230E',
+        'JJ4368K46M', 'JJG1203V17', 'JF88X641Q4', 'JF81F69G03', 'JJ0895GR64', 'JF7R6497Z8', 'JF2224L1G4', 'JF0561XP63',
+        'JF8P629R88', 'JF4F17929R', 'JFN65Y6867', 'JFY30S9478', 'JF6620J22G', 'JJG802I913', 'JJZ064416Q', 'JJ0G1431T0'
     ]
-    # c_no, s_no = 'panbao120', 'panbao120'
-    c_no, s_no = None, None
-    for i in sku_list:
-        order = transfer_data.create_cabinet_order(542, 542, 539, 539, i, "A", 1)  # 扫描当前sku并绑定到发货单，退税
-        time.sleep(5)
-        if c_no and s_no:
-            transfer_data.submit_cabinet_order(order, c_no, s_no)
-        else:
-            c_no, s_no = transfer_data.submit_cabinet_order(order)
-        # transfer_data.submit_cabinet_order(order)
+    transfer_data.generate_cds_cabin_order(sku_list=multi_sku_list, step=10)
