1e255833183c77f44c381bf88ecb7e6a1adbff24
xuhongwei <xuhongwei@popicorns.com>
2023-08-23 16:51:09 +0800

    流程节点组装及调用优化

---
+++ script/inventory_batch_init.py
@@ -0,0 +1,26 @@
+stock_config = [
+    {
+        "transfer": [
+            {
+                "qty": 5,
+                "trans_out_id": 512,
+                "trans_out_to_id": 0,
+                "trans_in_id": 565,
+                "trans_in_to_id": 0,
+                "inventory_type": 10,  # 现货10，在途20
+                "up_shelf_mode": "box",
+                "sale_sku": "HW4TF82839"
+            },
+            {
+                "qty": 5,
+                "trans_out_id": 512,
+                "trans_out_to_id": 0,
+                "trans_in_id": 565,
+                "trans_in_to_id": 0,
+                "inventory_type": 20,  # 现货10，在途20
+                "up_shelf_mode": "box",
+                "sale_sku": "HW4TF82839"
+            }
+        ]
+    }
+]
