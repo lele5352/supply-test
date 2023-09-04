743b0d4bfc3e024517c2f04deb6be9360e31f976
chung-jay <edy@edydeMacBook-Pro.local>
2023-09-04 10:40:12 +0800

    commit: redis操作方法

--- utils/redis_client.py
+++ utils/redis_client.py
@@ -35,7 +35,64 @@ class BaseRedisClient(Redis, ABC):
     @deserialize
     def get(self, name):
         """
+        操作string类型，获取值
         :param name: redis key name
         """
         return super().get(name)
+
+    def set(self, name, value, **kwargs):
+        """
+        操作string类型，写入值
+        :param name: redis key name
+        :param value: 值
+        """
+        return super().set(name, value, **kwargs)
+
+    def smembers(self, name: str):
+        """
+        获取set类型数据
+        :param name: redis key name
+        """
+        return super().smembers(name)
+
+    def sismember(self, name: str, value: str):
+        """
+        判断某个值是否存在set
+        :param name: redis key name
+        :param value: redis key value
+        """
+        return super().sismember(name, value)
+
+    def sadd(self, name: str, *values):
+        """
+        add to a set
+        :param name: redis key name
+        :param values: value
+        """
+        return super().sadd(name, *values)
+
+    def srem(self, name:str, *values):
+        """
+        remove from a set
+        :param name: redis key name
+        :param values: value
+        """
+        return super().srem(name, *values)
+
+    def delete(self, name):
+        """
+        删除 key
+        :param name: redis key name
+        """
+        return super().delete(name)
+
+    def hdel(self, name, *keys):
+        """
+        删除 hash key
+        :param name: redis key name
+        :param keys: hash key
+        """
+        return super().hdel(name, *keys)
+
+
 
